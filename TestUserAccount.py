3ca0f6b66303c7f8f887824f36dba8fde57d7eef
none <none>
2016-12-04 20:12:30 +0200

    comment

---
+++ TestUserAccount.py
@@ -0,0 +1,13 @@
+#This script performs some simple tests on the UserAccount class.
+import UserAccount
+
+#Three things are missing from the line below - fill them in
+my_user=UserAcount(  ,  ,  )
+
+#Call the print_secret method (function) - it takes one input - a guess for the password.
+
+#Use the wrong password as input here
+my_user.
+
+#Use the right password here
+my_user.
---
+++ UserAccount.py
@@ -0,0 +1,27 @@
+class UserAccount :
+    """
+    A class that models a user account for mega-social-media website, GuessMySecret
+    """
+
+    def __init__(self, username, password, secret):
+        
+        """
+        Initialize the UserAccount object:
+        :param username: user name (string)
+        :param password: password to gain access to account (string)
+        :param secret: user's secret, accessible only with password (string)
+        """
+        self.username=username
+        #Complete the __init__ method
+
+    def print_secret(self,password_attempt):
+        
+        """
+        Checks user input, password_attempt - if this matches the user's
+        password, then the user's secret is printed to the screen.
+
+        :param password_attempt: guess for the password (string)
+        """
+        #Complete this method - check the input variable, password_attempt,
+        #against the password for this user (the value of the instance variable, password).
+        #If, and only if, there's a match, then print the user's secret to the screen.
